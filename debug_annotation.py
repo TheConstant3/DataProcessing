2128973aa52e42ab034844a4ff9c86b2fde37c7f
TheConstant3 <46416203+TheConstant3@users.noreply.github.com>
2020-07-11 17:30:18 +0700

    fixed debug

--- debug_annotation.py
+++ debug_annotation.py
@@ -4,9 +4,12 @@ import pandas as pd
 import os
 import random
 
+# custom colors
+# COLORS = [(215,125,125),
+#           ...]
 
 class Debug:
-    def __init__(self, dataset='train', size=400):
+    def __init__(self, dataset='train', size=600):
         self.dataset = dataset
         if dataset not in ['train', 'validation', 'test']:
             raise ValueError('dataset must be train one of {}, not \"{}\"'.format(['train', 'validation',
@@ -15,9 +18,10 @@ class Debug:
         self.dir = 'Photos/Frags_{}/{}/'.format(size, dataset[0].upper()+dataset[1:])
         if dataset == 'validation':
             dataset = 'val'
-        classes = pd.read_csv('Annotations/classes.csv', header=None,
-                                names=['class', 'id'])
-        self.colors = [tuple(int(random.random() * 255) for _ in range(3))  for _ in range(len(classes['id']))]
+        classes = pd.read_csv('Annotations/classes.csv', header=None, names=['class_', 'id_'])
+        self.dict_classes = dict(zip(classes.class_, classes.id_))
+        self.colors = [tuple(int(random.random() * 255) for _ in range(3)) for _ in range(len(classes['id_']))]
+
         data = pd.read_csv('Annotations/Frags_{}/{}_annotations.csv'.format(size, dataset), header=None,
                             names=['path', 'x_min', 'y_min', 'x_max', 'y_max', 'label'])
 
@@ -35,17 +39,19 @@ class Debug:
         data = data.values.tolist()
         print('Frag has objects:', data[0] is not None)
         for row in data:
+            *coords, label = row
             try:
-                id = self.classes.loc[self.classes['class'] == row[4]]
+                id = self.dict_classes[label]
 
                 # for custom colors
                 # color = COLORS[int(id)]
 
-                # for random colors
+                # for random colors int(id)
                 color = self.colors[int(id)]
             except:
                 continue
-            draw.rectangle((int(row[0]), int(row[1]), int(row[2]), int(row[3])), outline=color, width=5)
+            # draw.rectangle((int(row[0]), int(row[1]), int(row[2]), int(row[3])), outline=color, width=5)
+            draw.rectangle(tuple(coords), outline=color, width=5)
         plt.figure(figsize=(20, 20))
         plt.imshow(image)
 
